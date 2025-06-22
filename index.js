eb716da8fead3faec31bcc8c5b52afa049ced648
Arno Christie <arno.christie@gmail.com>
2025-06-22 21:58:30 +0200

    Finished Request Header Parse Microservice Project
    Implemented all required API endpoints for the Request Header Parser Microservice project:
    
    - GET /api/whoami → returns device ip address, preferred language, and software.

--- index.js
+++ index.js
@@ -24,6 +24,18 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get("/api/whoami", (req, res) => {
+  const ip = req.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const lang = req.headers['accept-language'];
+  const sftware = req.header['user-agent'];
+
+  res.json({
+    ipaddress: ip,
+    language: lang,
+    software: sftware
+  });
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
